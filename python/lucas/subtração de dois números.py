a131f7c0c8c8c993a735fd91df2d41eaea8dce0f
lucasteodorodealmeida <101517072+lucasteodorodealmeida@users.noreply.github.com>
2022-04-01 12:56:43 -0300

    exercicios
    novos

---
+++ python/lucas/subtração de dois números.py
@@ -0,0 +1,4 @@
+print('ESTE PROGRAMA FAZ A SUBTRAÇÃO DE DOIS NÚMEROS');
+a=float(input('Digite o valor positivo (aquele do qual devemos subtrair) '));
+b=float(input('Digite o valor negativo(aquele que será subtraido) '));
+print(a-b);
